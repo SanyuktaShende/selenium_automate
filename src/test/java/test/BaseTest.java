9b8a406ff178dea6f5b67b836acab3f7e126f930
Sanyukta Shende <sanyuktashende7@gmail.com>
2026-02-01 16:30:07 +0530

    recent update: base test page

---
+++ src/test/java/test/BaseTest.java
@@ -0,0 +1,32 @@
+package test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+import org.testng.annotations.BeforeMethod;
+
+
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.get("https://thinking-tester-contact-list.herokuapp.com/");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
